caa30b62667bf652154cc3ff079314fd9b8307e9
baar-vendel <fhinnoigsr@gmail.com>
2022-06-11 14:05:07 +0900

    staticフォルダー増設

--- main.js
+++ main.js
@@ -2,4 +2,11 @@ const express = require("express");
 
 const app = express();
 app.use(express.static("static"))
+
+// middleware
+app.post("/whatisanswer", (request, response) =>
+{
+    response.send("ggrks");
+});
+
 app.listen(3000);
