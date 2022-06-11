847c8fb20fd2c2e806c54e28214f059d970ba83e
E-nana17 <104970804+E-nana17@users.noreply.github.com>
2022-06-11 15:04:50 +0900

    selectページを表示可能に

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
+const fs = require("fs");
 const express = require("express");
-
+const ejs = require("ejs");
 const app = express();
-app.use(express.static("static"))
+
+app.get("/", (request, response) => {
+    const template = fs.readFileSync("select.ejs", "utf-8");
+    const html = ejs.render(template, {
+    });
+    response.send(html);
+  });
 
 app.listen(3000);
