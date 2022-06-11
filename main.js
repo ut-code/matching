faaa56107a797811ad8f92eeb5f0ba036f5c1c9a
E-nana17 <104970804+E-nana17@users.noreply.github.com>
2022-06-11 16:57:04 +0900

    Wordクラス追加

--- main.js
+++ main.js
@@ -3,16 +3,30 @@ const express = require("express");
 const ejs = require("ejs");
 const app = express();
 
-class deck{
+class Deck{
     deckName;
     wordList = [];
+};
+
+class Word{
+    deutsch;
+    meaning;
+    date = new Date();
+    point = 0;
+
+    constructor(deutsch,meaning){
+        this.deutsch = deutsch;
+        this.meaning = meaning;
+    };
 };
 
 let deckList = [];
 
 //test用
-deckList.push(new deck());
+deckList.push(new Deck());
 deckList[0].deckName = "test";
+deckList[0].wordList.push(new Word("Nacht","night"));
+deckList[0].wordList.push(new Word("Morgen","morning"));
 //
 
 app.get("/", (request, response) => {
@@ -21,13 +35,13 @@ app.get("/", (request, response) => {
         deckList: deckList,
     });
     response.send(html);
-  });
+});
 
 app.post("/practice", (request, response) => {
     const template = fs.readFileSync("practice.ejs", "utf-8");
     const html = ejs.render(template, {
     });
     response.send(html);
-  });
+});
 
 app.listen(3000);
