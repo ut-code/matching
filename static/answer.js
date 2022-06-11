02fbbcc39aad5ff74e54070cd079d736df224f6b
baar-vendel <fhinnoigsr@gmail.com>
2022-06-11 17:47:28 +0900

    siro

--- static/answer.js
+++ static/answer.js
@@ -4,3 +4,6 @@ document.getElementById("see_answer").onclick = () =>
     document.getElementById("see_answer").style.display = "none";
     document.getElementById("item").textContent="夜";
 };
+
+// スペースキーで次行けるようにする
+// document.addEventListener('キーイベント', イベント発生時に実行する関数);
