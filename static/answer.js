b81e86383e1866bd566cf500722b799d0f1c105d
E-nana17 <104970804+E-nana17@users.noreply.github.com>
2022-06-11 17:39:28 +0900

    a

---
+++ static/answer.js
@@ -0,0 +1,22 @@
+document.getElementById("see_answer").onclick = () => 
+{
+  document.getElementById("evals").style.display = "block";
+  document.getElementById("see_answer").style.display = "none";
+  document.getElementById("item").textContent="夜";
+};
+
+// document.getElementById("eval-1").onclick = () => 
+// {
+//   deckList[0].wordList.date = new Date();
+//   deckList[0].wordList.point += -1;
+// };
+// document.getElementById("eval0").onclick = () => 
+// {
+//   deckList[0].wordList.date = new Date();
+//   deckList[0].wordList.point += 0;
+// };
+// document.getElementById("eval1").onclick = () => 
+// {
+//   deckList[0].wordList.date = new Date();
+//   deckList[0].wordList.point += 1;
+// };
