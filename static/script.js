7ee5dbd7847eef01227370f3937770c25dcedfc6
chvmvd <104971044+chvmvd@users.noreply.github.com>
2022-06-12 17:05:29 +0900

    微調整

--- static/script.js
+++ static/script.js
@@ -93,50 +93,6 @@ for(let i=0;i<3;i++){
 wordId = chooseWordId();
 setproblem();
 
-
-
-
-
-
-
-
-
-// import question from "practice.ejs";
-/*
-for(let i=0;i<3;i++){
-    document.getElementsByClassName("eval")[i].onclick = async() =>
-    {
-        document.getElementById("evals").style.display = "none";
-        document.getElementById("see_answer").style.display = "block";
-        document.getElementById("item").textContent="Nacht";
-
-        const json = JSON.stringify({ eval: i});//i=0 => ×  i=1 => △  i=2 => 〇
-        const response = await fetch("/eval_response", {
-          method: "post",
-          headers: { "Content-Type": "application/json" },
-          body: json,
-        });
-    }
-}
-*/
-// document.getElementById("eval-1").onclick = () => 
-// {
-//   deckList[0].wordList.date = new Date();
-//   deckList[0].wordList.point += -1;
-// };
-// document.getElementById("eval0").onclick = () => 
-// {
-//   deckList[0].wordList.date = new Date();
-//   deckList[0].wordList.point += 0;
-// };
-// document.getElementById("eval1").onclick = () => 
-// {
-//   deckList[0].wordList.date = new Date();
-//   deckList[0].wordList.point += 1;
-// };
-
-
-
 window.onkeyup = keyup;
 // document.body.onkeyup = keyup;
 function keyup(e)
