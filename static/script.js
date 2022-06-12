7e9d690f5fdbe50a12f61926e8df0bc47fa5532a
E-nana17 <104970804+E-nana17@users.noreply.github.com>
2022-06-12 14:30:23 +0900

    ○△×を押すと次の問題表示/バグ応急処置

--- static/script.js
+++ static/script.js
@@ -5,6 +5,7 @@ const deckId = document.getElementById("deckId").title;
 function chooseWordId(){
     return 0;
 }
+//問題のIDを保持しておく変数
 let wordId = chooseWordId();
 
 //問題を表示する関数
@@ -15,21 +16,36 @@ async function setproblem(){
     return;
 }
 
-//答えを表示する関数
-async function setanswer(){
+//答えを見るボタンが押された時の処理
+document.getElementById("see_answer").onclick = async() =>{
     const body = new URLSearchParams({deckId: deckId, wordId: wordId, type: 1});
     const response = await fetch("/getword", {method: "post", body: body});
     document.getElementById("item").textContent = await response.text();
 
-    //答えを見るボタン削除
+    //ボタンを○△×に切り替え
     document.getElementById("evals").style.display = "block";
     document.getElementById("see_answer").style.display = "none";
     return;
 }
 
-//答えを見るボタンを押すと答えが表示される
-document.getElementById("see_answer").onclick = setanswer;
+//○△×が押された時の処理
+for(let i=0;i<3;i++){
+    document.getElementsByClassName("eval")[i].onclick = async() =>
+    {
+        //評価を記録
+        const body = new URLSearchParams({deckId: deckId, wordId: wordId, point: i});
+        await fetch("/eval_response", {method: "post", body: body});
 
+        //次の問題を出す
+        wordId = chooseWordId();
+        setproblem();
 
-setproblem(0); //最初の問題表示
+        //ボタンを「答えを見る」に切り替え
+        document.getElementById("evals").style.display = "none";
+        document.getElementById("see_answer").style.display = "block";
+        return;
+    }
+}
+
+setproblem(); //最初の問題表示
 
